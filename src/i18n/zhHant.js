1e2fd908078828ae8a9589005e70badb27341be1
Hengll <heeric891120@gmail.com>
2025-01-17 16:50:09 +0800

    更新

---
+++ src/i18n/zhHant.js
@@ -0,0 +1,21 @@
+export default {
+  api: {
+    userNotFound: '用戶不存在',
+    userAccountRequired: '用戶帳號必填',
+    userAccountTooShort: '用戶帳號太短',
+    userAccountTooLong: '用戶帳號太長',
+    userAccountDuplicate: '用戶帳號重複',
+    userAccountInvalid: '用戶帳號只能是字母或數字',
+    userPasswordRequired: '用戶密碼必填',
+    userPasswordIncorrect: '密碼不符',
+    userEmailRequired: '用戶信箱必填',
+    userEmailInvalid: '用戶信箱格式不符',
+    userEmailDuplicate: '用戶信箱重複',
+    userPasswordTooShort: '用戶密碼太短',
+    userPasswordTooLong: '用戶密碼太長',
+    userUserNameRequired: '用戶名稱必須',
+    userUserNameDuplicate: '用戶名稱重複',
+    requestFormatError: '請求格式錯誤',
+    serverError: '伺服器錯誤'
+  }
+}

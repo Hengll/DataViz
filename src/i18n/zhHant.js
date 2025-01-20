f12cae743bdcba3af51e09886fbb26c0903cb9fb
Hengll <heeric891120@gmail.com>
2025-01-20 16:39:48 +0800

    更新

--- src/i18n/zhHant.js
+++ src/i18n/zhHant.js
@@ -8,6 +8,8 @@ export default {
     userAccountInvalid: '用戶帳號只能是字母或數字',
     userPasswordRequired: '用戶密碼必填',
     userPasswordIncorrect: '密碼不符',
+    userTokenInvalid: '使用者驗證錯誤',
+    userTokenExpired: '登入過期',
     userEmailRequired: '用戶信箱必填',
     userEmailInvalid: '用戶信箱格式不符',
     userEmailDuplicate: '用戶信箱重複',
@@ -16,6 +18,12 @@ export default {
     userUserNameRequired: '用戶名稱必須',
     userUserNameDuplicate: '用戶名稱重複',
     requestFormatError: '請求格式錯誤',
+    uploadFailed: '圖片上傳失敗',
+    userPermissionDenied: '用戶權限不足',
+    dataNameRequired: '資料集名稱必填',
+    dataRequired: '資料集需上傳',
+    dataFormatError: '資料格式錯誤',
+    userIdRequired: '用戶ID需要',
     serverError: '伺服器錯誤'
   }
 }
